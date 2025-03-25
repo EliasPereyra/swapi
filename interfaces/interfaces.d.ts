0a949ebc245a8244225c4552747e549644308780
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-03-24 23:13:39 -0300

    Chore: create an interfaces file to define three interfaces

---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,52 @@
+interface Person {
+  name: string;
+  height: string;
+  mass: string;
+  hairColor: string;
+  skinColor: string;
+  eyeColor: string;
+  birthYear: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+interface Film {
+  title: string;
+  episodeId: number;
+  openingCrawl: string;
+  director: string;
+  producer: string;
+  releaseDate: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+interface Planet {
+  name: string;
+  rotationPeriod: string;
+  orbitalPeriod: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surfaceWater: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
