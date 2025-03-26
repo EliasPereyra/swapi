a04e1e6b35c294dc471092506be3f688dc3b9f5c
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-03-26 16:22:33 -0300

    Create a general icon object for exporting different icons

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import planet from "@/assets/icons/planet.png";
+import vehicle from "../assets/icons/vehicle.png";
+import starship from "../assets/icons/spaceship.png";
+import species from "../assets/icons/animal-footprints.png";
+import search from "../assets/icons/search.png";
+
+export const icons = {
+  planet,
+  vehicle,
+  starship,
+  species,
+  search,
+};
