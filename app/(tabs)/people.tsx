83ac1ba967b54dd4a899fdf7764487f46b9ef1e5
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-03-26 23:08:43 -0300

    Fix: update some ui colors

--- app/(tabs)/people.tsx
+++ app/(tabs)/people.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ParallaxScrollView from "@/components/templates/ParallaxScrollView";
-import Person from "@/components/molecules/PersonCard";
+import PersonCard from "@/components/molecules/PersonCard";
 import Search from "@/components/atoms/Search";
 import { usePeople } from "@/hooks/usePeople";
 import LoadingMessage from "@/components/atoms/LoadingMessage";
+import ErrorMessage from "@/components/atoms/ErrorMessage";
+import { Colors } from "@/constants/Colors";
 
 export default function People() {
   const { isPending, error, data } = usePeople();
   const [name, setName] = useState("");
-  const [filteredPeople, setFilteredPeople] = useState<Person[]>(data?.results);
+  const [filteredPeople, setFilteredPeople] = useState<Person[]>(data.results);
 
   useEffect(() => {
     if (name) {
@@ -25,23 +27,25 @@ export default function People() {
 
   if (isPending) return <LoadingMessage message="Cargando personajes..." />;
 
+  if (error) return <ErrorMessage message={error.message} />;
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#474108" }}
     >
       <Text style={styles.title}>Personajes</Text>
       <Text style={styles.description}>Personajes del Star Wars universe</Text>
       <Search value={name} onChange={setName} />
 
       <View style={styles.peopleList}>
-        {filteredPeople.length > 0 ? (
+        {filteredPeople?.length > 0 ? (
           filteredPeople.map((person: Person) => (
-            <Person key={person.name} person={person} />
+            <PersonCard key={person.name} person={person} />
           ))
         ) : (
           <View style={styles.noResults}>
             <Text style={styles.noResultsText}>
-              No se encontraron resultados. Intente nuevamente con otro nombre.
+              No se encontraron resultados.
             </Text>
           </View>
         )}
@@ -53,18 +57,19 @@ export default function People() {
 const styles = StyleSheet.create({
   title: {
     fontSize: 32,
-    color: "#FFF",
+    color: Colors.dark.primary,
     textAlign: "center",
     marginBottom: 8,
   },
   description: {
     fontSize: 16,
-    color: "#FFF",
+    color: "#ddd",
     textAlign: "center",
   },
   peopleList: {
     marginTop: 20,
     display: "flex",
+    justifyContent: "space-between",
     flexDirection: "row",
     flexWrap: "wrap",
     gap: 16,
